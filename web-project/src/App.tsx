f0473df79443cc60d4821f225bb1f6a4c5d6db4a
TudorMurariu <tudorcristianmurariu@yahoo.com>
2025-01-03 01:32:09 +0200

    added routing

--- web-project/src/App.tsx
+++ web-project/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LogIn from './pages/LogIn'
+import MainPage from './pages/MainPage'
+import ItemPage from './pages/ItemPage'
 
 function App() {
   return (
-    <LogIn/>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<LogIn />} />
+          <Route path="home" element={<MainPage />} />
+          <Route path="user/:name" element={<ItemPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
